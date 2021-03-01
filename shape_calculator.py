adb46444a607cc0dd105dc616110835a43103174
gtburchett <gburchett88@gmail.com>
2021-03-01 08:54:44 -0600

    Finished creating all methods for the parent Rectangle class.

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,32 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+        
+    def set_height(self, new_height):
+        self.height = new_height
 
+    def set_width(self, new_width):
+        self.width = new_width
 
+    def get_area(self):
+        return self.height * self.width
 
+    def get_perimeter(self):
+        return 2 * self.height + 2 * self.width
 
-class Square:
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        for _ in range(self.height):
+            return '*' * self.width
+
+    def get_amount_inside(self, other):
+        return int(self.get_area() / other.get_area())
+
+class Square(Rectangle):
+    pass
